3ab2d6bcb97faf216db9e21c4f8fe744f07dc908
ABammann <87564425+ABammann@users.noreply.github.com>
2021-09-10 16:05:13 +0200

    got weekly overview to appear

--- script.js
+++ script.js
@@ -81,6 +81,7 @@ function displayWeatherCondition(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 
 function displayForecast(response) {
@@ -110,8 +111,6 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "c9b6f1c11dca5098f50e3c467f7b1552";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
-
-displayForecast();
